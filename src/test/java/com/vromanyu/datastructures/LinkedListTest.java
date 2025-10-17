736f33111a26f7db5f8647bac0c08349c6f38283
vromanyu <vromanyu@outlook.com>
2025-10-17 12:04:33 +0300

    test append method

--- src/test/java/com/vromanyu/datastructures/LinkedListTest.java
+++ src/test/java/com/vromanyu/datastructures/LinkedListTest.java
@@ -1,5 +1,6 @@
 package com.vromanyu.datastructures;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class LinkedListTest {
@@ -11,6 +12,14 @@ class LinkedListTest {
         linkedList.printHead();
         linkedList.printTail();
         linkedList.printLength();
+
+        Assertions.assertAll(() -> {
+            Assertions.assertNotNull(linkedList);
+            Assertions.assertNotNull(linkedList.getHead());
+            Assertions.assertNotNull(linkedList.getTail());
+            Assertions.assertEquals(10, linkedList.getHead().getValue());
+            Assertions.assertEquals(10, linkedList.getTail().getValue());
+        });
     }
 
     @Test
@@ -20,5 +29,37 @@ class LinkedListTest {
         linkedList.printHead();
         linkedList.printTail();
         linkedList.printLength();
+
+        Assertions.assertAll(() -> {
+            Assertions.assertNotNull(linkedList);
+            Assertions.assertNull(linkedList.getHead());
+            Assertions.assertNull(linkedList.getTail());
+            Assertions.assertEquals(0, linkedList.getLength());
+        });
+    }
+
+    @Test
+    public void testAppendToEmptyLinkedList(){
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.append(10);
+
+        Assertions.assertAll(() -> {
+            Assertions.assertEquals(10, linkedList.getHead().getValue());
+            Assertions.assertEquals(10, linkedList.getTail().getValue());
+            Assertions.assertEquals(1, linkedList.getLength());
+        });
+    }
+
+    @Test
+    public void testAppendToNotEmptyLinkedList(){
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        int value = 10;
+        linkedList.append(value);
+
+        Assertions.assertAll(() -> {
+            Assertions.assertNotNull(linkedList.getHead());
+            Assertions.assertNotNull(linkedList.getTail());
+            Assertions.assertEquals(value, linkedList.getHead().getValue());
+        });
     }
 }
